536e38f84a459ae1b5253e180ae79e4518c2380d
lev872 <luisev872@gmail.com>
2024-02-12 08:06:19 +0100

    añadido LITCADENA a imprime en DomJudge.java

--- implementacion_jflex/src/DomJudge.java
+++ implementacion_jflex/src/DomJudge.java
@@ -1,8 +1,7 @@
 import alex.ALexOperations.ECaracterInesperado;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+
+import java.io.*;
+
 import alex.AnalizadorLexicoTiny;
 import alex.ClaseLexica;
 import alex.UnidadLexica;
@@ -10,13 +9,13 @@ import alex.UnidadLexica;
 public class DomJudge {
     private static void imprime(UnidadLexica unidad) {
         switch(unidad.clase()) {
-            case IDEN: case LITENT: case LITREAL: System.out.println(unidad.lexema()); break;
+            case IDEN: case LITENT: case LITREAL: case LITCADENA: System.out.println(unidad.lexema()); break;
             default: System.out.println(unidad.clase().getImage());
         }
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Reader input  = new InputStreamReader(System.in);
+        Reader input  = new InputStreamReader(new FileInputStream("implementacion_jflex/src/alex/input.txt"));
         AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
         UnidadLexica unidad = null;
         do {
