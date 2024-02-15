9fe03fa52ff6ee739fd1a49cdbbb880825ea7d38
ginacassin <ginacassin47@gmail.com>
2024-02-15 13:43:26 +0100

    feat: actualizar domjudge clases segun ej profe

--- implementacion_jflex/src/DomJudge.java
+++ implementacion_jflex/src/DomJudge.java
@@ -1,7 +1,8 @@
 import alex.ALexOperations.ECaracterInesperado;
-
-import java.io.*;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import alex.AnalizadorLexicoTiny;
 import alex.ClaseLexica;
 import alex.UnidadLexica;
@@ -15,18 +16,21 @@ public class DomJudge {
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Reader input  = new InputStreamReader(new FileInputStream("implementacion_jflex/src/alex/input.txt"));
+        Reader input  = new InputStreamReader(System.in);
         AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
         UnidadLexica unidad = null;
+        boolean error;
         do {
+            error = false;
             try {
                 unidad = al.yylex();
                 imprime(unidad);
             }
             catch(ECaracterInesperado e) {
                 System.out.println("ERROR");
+                error = true;
             }
         }
-        while (unidad == null || unidad.clase() != ClaseLexica.EOF);
+        while (error || unidad.clase() != ClaseLexica.EOF);
     }
 } 
--- implementacion_manual/src/DomJudge.java
+++ implementacion_manual/src/DomJudge.java
@@ -1,19 +1,18 @@
-
+import alex.AnalizadorLexicoTiny.ECaracterInesperado;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
 import alex.AnalizadorLexicoTiny;
 import alex.ClaseLexica;
 import alex.UnidadLexica;
-import alex.AnalizadorLexicoTiny.ECaracterInesperado;
 
 public class DomJudge {
     private static void imprime(UnidadLexica unidad) {
         switch(unidad.clase()) {
             case IDEN: case ENT: case REAL: System.out.println(unidad.lexema()); break;
-                      default: System.out.println(unidad.clase().getImage());
+            default: System.out.println(unidad.clase().getImage());
         }
     }
 
@@ -21,15 +20,19 @@ public class DomJudge {
         Reader input  = new InputStreamReader(System.in);
         AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
         UnidadLexica unidad = null;
+        boolean error;
         do {
+            error = false;
             try {
                 unidad = al.sigToken();
                 imprime(unidad);
             }
             catch(ECaracterInesperado e) {
                 System.out.println("ERROR");
+                error = true;
             }
         }
-        while (unidad == null || unidad.clase() != ClaseLexica.EOF);
+        while (error || unidad.clase() != ClaseLexica.EOF);
     }
+
 } 
