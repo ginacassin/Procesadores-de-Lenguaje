7ca87df3fe358df2cfac52ae408d500418229fa4
robuso <robuso02@gmail.com>
2024-04-05 17:35:11 +0200

    ClaseSemanticaTiny done

---
+++ fase 3/asint/ClaseSemanticaTiny.java
@@ -0,0 +1,33 @@
+package asint;
+
+public class ClaseSemanticaTiny extends SintaxisAbstractaTiny {
+    public ClaseSemanticaTiny() {
+        super();
+    }
+    public Exp mkop1(String op, Exp opnd1, Exp opnd2) {
+        switch(op) {
+            case "<": return menor(opnd1,opnd2);
+            case ">": return mayor(opnd1,opnd2);
+            case "<=": return menor_igual(opnd1,opnd2);
+            case ">=": return mayor_igual(opnd1,opnd2);
+            case "==": return igual(opnd1,opnd2);
+            case "!=": return no_igual(opnd1,opnd2);
+            default: throw new UnsupportedOperationException("Bad op");
+        }
+    }
+    public Exp mkop4(String op, Exp opnd1, Exp opnd2) {
+        switch(op) {
+            case "*": return mul(opnd1,opnd2);
+            case "/": return div(opnd1,opnd2);
+            case "%": return mod(opnd1,opnd2);
+            default: throw new UnsupportedOperationException("Bad op");
+        }
+    }
+    public Exp mkop5(String op, Exp opnd) {
+        switch(op) {
+            case "-": return neg(opnd);
+            case "not": return not(opnd);
+            default: throw new UnsupportedOperationException("Bad op");
+        }
+    }
+}
