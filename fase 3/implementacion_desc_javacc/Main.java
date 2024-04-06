6002b2f793d4b6735e1f1930015a12af97bb1f0e
Gina Cassin <ginacass@ucm.es>
2024-04-06 23:33:22 +0200

    fix: bloque

---
+++ fase 3/implementacion_desc_javacc/Main.java
@@ -0,0 +1,9 @@
+package implementacion_desc_javacc;
+import java.io.FileReader;
+public class Main{
+    public static void main(String[] args) throws Exception {
+        ConstructorASTsTiny asint = new ConstructorASTsTiny(new FileReader(args[0]));
+        asint.disable_tracing();
+        System.out.println(asint.analiza());
+    }
+}
