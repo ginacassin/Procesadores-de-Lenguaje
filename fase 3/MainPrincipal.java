c032516249644e624e32f2878c31234eacec763d
santiest <santiago.rabbia@gmail.com>
2024-04-09 21:18:03 +0200

    Mejor main principal (le falta terminar bien el javacc)

--- fase 3/MainPrincipal.java
+++ fase 3/MainPrincipal.java
@@ -2,6 +2,7 @@ import asint.SintaxisAbstractaTiny.*;
 import c_ast_ascendente.AnalizadorLexicoTiny;
 import c_ast_descendente.ConstructorASTsTiny;
 import c_ast_descendente.ConstructorASTsTinyDJ;
+import evaluador.recursivo.EvaluadorRecursivo;
 import evaluador.visitante.Impresion;
 
 import java.io.FileInputStream;
@@ -11,9 +12,9 @@ import java.io.Reader;
 
 public class MainPrincipal {
     public static void main(String[] args) throws Exception {
-        FileInputStream fis = new FileInputStream(args[1]);
-        //char c = (char) fis.read();
-        if (args[0].equals("asc")) {
+        FileInputStream fis = new FileInputStream(args[0]);
+        char c = (char) fis.read();
+        if (c == 'a') {
             Reader input = new InputStreamReader(fis);
             AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
             c_ast_ascendente.ConstructorASTTiny asint = new c_ast_ascendente.ConstructorASTTinyDJ(alex);
@@ -21,10 +22,10 @@ public class MainPrincipal {
             // asint.debug_parse();
             System.out.println("CONSTRUCCION AST ASCENDENTE");
             Prog prog = (Prog)asint.debug_parse().value;
-            // System.out.println("IMPRESION RECURSIVA");
-            // new EvaluadorRecursivo().muestraPrograma(prog);
-            // System.out.println("IMPRESION INTERPRETE");
-            // prog.imprime();
+            System.out.println("IMPRESION RECURSIVA");
+            new EvaluadorRecursivo().muestraPrograma(prog);
+            System.out.println("IMPRESION INTERPRETE");
+            prog.imprime();
             System.out.println("IMPRESION VISITANTE");
             prog.procesa(new Impresion());
 
@@ -33,20 +34,22 @@ public class MainPrincipal {
             ConstructorASTsTiny asint = new ConstructorASTsTinyDJ(new InputStreamReader(fis));
             asint.disable_tracing();
             System.out.println("CONSTRUCCION AST DESCENDENTE");
-            Prog p = asint.analiza();
+            Prog prog = asint.analiza();
 
-            fis = new FileInputStream(args[1]);
-            fis.read();
-            asint = new ConstructorASTsTiny(fis);
-            asint.disable_tracing();
+            // fis = new FileInputStream(args[1]);
+            // fis.read();
+            // asint = new ConstructorASTsTiny(fis);
+            // asint.disable_tracing();
             System.out.println("IMPRESION RECURSIVA");
+            asint.ReInit();
             asint.analiza().imprime();
             System.out.println("IMPRESION INTERPRETE");
+            prog.imprime();
 
             System.out.println("IMPRESION VISITANTE");
-            asint = new ConstructorASTsTiny(new FileReader(args[1]));
-            asint.disable_tracing();
-            asint.analiza().procesa(new Impresion());
+            // asint = new ConstructorASTsTiny(new FileReader(args[0]));
+            // asint.disable_tracing();
+            prog.procesa(new Impresion());
         }
     }
 }
