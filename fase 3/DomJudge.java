9f90166f518b962fec86e25209670c1b91df43cf
santiest <santiago.rabbia@gmail.com>
2024-04-09 22:07:40 +0200

    Fix main principal y domjudge

--- fase 3/DomJudge.java
+++ fase 3/DomJudge.java
@@ -3,8 +3,12 @@ import java.io.Reader;
 
 import asint.SintaxisAbstractaTiny.*;
 import c_ast_ascendente.AnalizadorLexicoTiny;
+import c_ast_ascendente.GestionErroresTiny.ErrorLexico;
+import c_ast_ascendente.GestionErroresTiny.ErrorSintactico;
 import c_ast_descendente.ConstructorASTsTiny;
 import c_ast_descendente.ConstructorASTsTinyDJ;
+import c_ast_descendente.ParseException;
+import c_ast_descendente.TokenMgrError;
 import evaluador.recursivo.ImpresorRecursivo;
 import evaluador.visitante.Impresion;
 
@@ -12,12 +16,23 @@ public class DomJudge {
     public static void main(String[] args) throws Exception {
         Reader input = new InputStreamReader(System.in);
         char c = (char) input.read();
+        Prog prog;
         if (c == 'a') {
             AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
             c_ast_ascendente.ConstructorASTTiny asint = new c_ast_ascendente.ConstructorASTTinyDJ(alex);
 
             System.out.println("CONSTRUCCION AST ASCENDENTE");
-            Prog prog = (Prog)asint.debug_parse().value;
+            try {
+                prog = (Prog)asint.debug_parse().value;
+            }
+            catch(ErrorLexico e) {
+                System.out.println("ERROR_LEXICO");
+                return;
+            }
+            catch(ErrorSintactico e) {
+                System.out.println("ERROR_SINTACTICO");
+                return;
+            }
 
             System.out.println("IMPRESION RECURSIVA");
             new ImpresorRecursivo().muestraPrograma(prog);
@@ -32,7 +47,17 @@ public class DomJudge {
             ConstructorASTsTiny asint = new ConstructorASTsTinyDJ(input);
             asint.disable_tracing();
             System.out.println("CONSTRUCCION AST DESCENDENTE");
-            Prog prog = asint.analiza();
+            try {
+                prog = asint.analiza();
+            }
+            catch(ParseException e) {
+                System.out.println("ERROR_SINTACTICO");
+                return;
+            }
+            catch(TokenMgrError e) {
+                System.out.println("ERROR_LEXICO");
+                return;
+            }
 
             System.out.println("IMPRESION RECURSIVA");
             new ImpresorRecursivo().muestraPrograma(prog);
--- fase 3/MainPrincipal.java
+++ fase 3/MainPrincipal.java
@@ -11,37 +11,47 @@ import java.io.Reader;
 
 public class MainPrincipal {
     public static void main(String[] args) throws Exception {
-        FileInputStream fis = new FileInputStream(args[0]);
-        char c = (char) fis.read();
-        if (c == 'a') {
+        if (args.length < 3) {
+            System.err.println("Usage: java Main <archivo> <op> <opp>");
+            System.exit(1);
+        }
+
+        String archivo = args[0];
+        String op = args[1]; // Indica el constructor de ASTs
+        String opp = args[2]; // Estilo de procesamiento
+
+        FileInputStream fis = new FileInputStream(archivo);
+        Prog prog;
+        
+        if (op.equals("asc")) {
             Reader input = new InputStreamReader(fis);
             AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
             c_ast_ascendente.ConstructorASTTiny asint = new c_ast_ascendente.ConstructorASTTinyDJ(alex);
 
             System.out.println("CONSTRUCCION AST ASCENDENTE");
-            Prog prog = (Prog)asint.debug_parse().value;
-
-            System.out.println("IMPRESION RECURSIVA");
-            new ImpresorRecursivo().muestraPrograma(prog);
-
-            System.out.println("IMPRESION INTERPRETE");
-            prog.imprime();
-
-            System.out.println("IMPRESION VISITANTE");
-            prog.procesa(new Impresion());
+            prog = (Prog)asint.debug_parse().value;
         }
-        else {
+        else if (op.equals("desc")) {
             ConstructorASTsTiny asint = new ConstructorASTsTinyDJ(new InputStreamReader(fis));
             asint.disable_tracing();
             System.out.println("CONSTRUCCION AST DESCENDENTE");
-            Prog prog = asint.analiza();
+            prog = asint.analiza();
 
+        } else {
+            System.err.println("Constructor de ASTs no válido");
+            return;
+        }
+        
+        // Impresion del procesamiento seleccionado
+        if (opp.equals("rec")) {
             System.out.println("IMPRESION RECURSIVA");
             new ImpresorRecursivo().muestraPrograma(prog);
-
+        }
+        else if (opp.equals("int")) {
             System.out.println("IMPRESION INTERPRETE");
             prog.imprime();
-
+        }
+        else if (opp.equals("vis")) {
             System.out.println("IMPRESION VISITANTE");
             prog.procesa(new Impresion());
         }
