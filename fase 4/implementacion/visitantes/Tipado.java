a78bae5295e878f6d4caf1080435232df4a1b9c3
Gina Cassin <ginacass@ucm.es>
2024-05-05 14:41:07 +0200

    feat: ref y es_designador subido a ProcesamientoDef

--- fase 4/implementacion/visitantes/Tipado.java
+++ fase 4/implementacion/visitantes/Tipado.java
@@ -68,20 +68,6 @@ public class Tipado extends ProcesamientoDef {
             return new TipoOK();
         return new TipoError();
     }
-
-    private static T ref(T n) {
-        if (n instanceof Identificador) {
-            return ref(((DecTipo)n.getVinculo()).getTipo());
-        }
-        return n.getTipado();
-    }
-
-    private static boolean es_designador(Exp e) {
-        return e instanceof Iden
-            || e instanceof Acceso
-            || e instanceof Index
-            || e instanceof Indireccion;
-    }
     
     private static Set<String> theta;
 
