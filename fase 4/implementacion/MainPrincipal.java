575fe1eeef1b59939d71a513097cc528b6f2ffea
robuso <robuso02@gmail.com>
2024-05-05 17:18:47 +0200

    cambios a main principal

--- fase 4/implementacion/MainPrincipal.java
+++ fase 4/implementacion/MainPrincipal.java
@@ -4,6 +4,8 @@ import c_ast_descendente.ConstructorASTsTiny;
 import c_ast_descendente.ConstructorASTsTinyDJ;
 import evaluador.recursivo.ImpresorRecursivo;
 import evaluador.visitante.Impresion;
+import maquinaP.MaquinaP;
+import visitantes.*;
 
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -11,18 +13,17 @@ import java.io.Reader;
 
 public class MainPrincipal {
     public static void main(String[] args) throws Exception {
-        if (args.length < 3) {
-            System.err.println("Usage: java Main <archivo> <op> <opp>");
+        if (args.length < 2) {
+            System.err.println("Usage: java Main <archivo> <op>"); //op es desc o asc
             System.exit(1);
         }
 
         String archivo = args[0];
         String op = args[1]; // Indica el constructor de ASTs
-        String opp = args[2]; // Estilo de procesamiento
 
         FileInputStream fis = new FileInputStream(archivo);
         Prog prog;
-        
+
         if (op.equals("asc")) {
             Reader input = new InputStreamReader(fis);
             AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
@@ -41,19 +42,24 @@ public class MainPrincipal {
             System.err.println("Constructor de ASTs no válido");
             return;
         }
-        
-        // Impresion del procesamiento seleccionado
-        if (opp.equals("rec")) {
-            System.out.println("IMPRESION RECURSIVA");
-            new ImpresorRecursivo().muestraPrograma(prog);
-        }
-        else if (opp.equals("int")) {
-            System.out.println("IMPRESION INTERPRETE");
-            prog.imprime();
-        }
-        else if (opp.equals("vis")) {
-            System.out.println("IMPRESION VISITANTE");
-            prog.procesa(new Impresion());
-        }
+
+        // Proceso de vinculacion
+        prog.procesa(new VinculacionPrimera());
+
+        // Proceso de comprobacion de tipos
+        prog.procesa(new Tipado());
+
+        // Proceso de asignacion de espacio
+        prog.procesa(new AsignacionEspacioPrimera());
+
+        MaquinaP maquinaP = new MaquinaP(1, 1, 1, 1);
+
+        // Proceso etiquetado
+        prog.procesa(new Etiquetado(maquinaP));
+
+        // Proceso de generacion de codigo
+        prog.procesa(new GeneracionCodigo(maquinaP));
+
+        maquinaP.ejecuta();
     }
 }
